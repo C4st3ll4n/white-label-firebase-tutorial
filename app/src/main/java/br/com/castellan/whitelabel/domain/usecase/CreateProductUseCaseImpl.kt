8e24e217031f4047840d03cd3451b57e7b21db00
p_souza <psouza@braspag.com.br>
2021-09-08 03:38:46 -0300

    Add create product implementation

---
+++ app/src/main/java/br/com/castellan/whitelabel/domain/usecase/CreateProductUseCaseImpl.kt
@@ -0,0 +1,21 @@
+package br.com.castellan.whitelabel.domain.usecase
+
+import android.net.Uri
+import br.com.castellan.whitelabel.data.ProductRepository
+import br.com.castellan.whitelabel.domain.model.Product
+import java.util.*
+
+class CreateProductUseCaseImpl(
+    private val uploadProductImageUseCase: UploadProductImageUseCase,
+    private val productRepository: ProductRepository
+) : CreateProductUseCase {
+    override suspend fun invoke(description: String, price: Double, imageUri: Uri): Product {
+        return try {
+            val imageUrl = uploadProductImageUseCase(imageUri)
+            val product = Product(UUID.randomUUID().toString(), description, price, imageUrl)
+            productRepository.createProduct(product)
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
